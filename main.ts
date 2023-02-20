35bd3f281bd2c51fcce77a5ae569f73aefb8da81
mesa6-hugo-laura-y-bintea <122781476+mesa6-hugo-laura-y-bintea@users.noreply.github.com>
2023-02-20 10:00:12 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    factor_1 += randint(0, 9)
+    factor_2 += randint(0, 9)
+    basic.showNumber(factor_1)
+})
+let factor_2 = 0
+let factor_1 = 0
+basic.showString("Tablas multiplicar")
+basic.clearScreen()
+basic.forever(function () {
+	
+})
